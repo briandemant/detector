4f40703d421f605da983676792baf40a9c354c9a
Brian Demant <brde@fynskemedier.dk>
2012-12-21 10:26:59 +0100

    added commandline options and support for cluster

--- src/server.js
+++ src/server.js
@@ -6,9 +6,57 @@ var UaParser = require("./ua_parser.js");
 
 var uaParser = new UaParser();
 
-app.get('/', function (req, res) {
-	var ua = uaParser.parse(req.query.ua|| req.headers['user-agent']);
-	res.send(ua);
-});
+var cluster = require('cluster');
 
-app.listen(3000);
+var optimist = require('optimist')
+		.usage('Usage: $0 [-port num] [-forks num]')
+		.options('p', {
+			         alias   : 'port',
+			         default : 7101,
+			         describe: 'Which port to listen on'
+		         })
+		.options('f', {
+			         alias   : 'forks',
+			         default : 2,
+			         describe: 'How many forks of the process should be made'
+		         })
+		.options('h', {
+			         alias   : 'help',
+			         describe: 'you need help explained?'
+		         });
+;
+
+var port = optimist.argv.port;
+var forks = optimist.argv.forks;
+
+if (optimist.argv.h) {
+	optimist.showHelp();
+	process.exit();
+}
+
+
+if (cluster.isMaster) {
+	for (var i = 0; i < forks; ++i) {
+		cluster.fork();
+	}
+} else {
+
+	app.get('/', function (req, res) {
+		var ua = uaParser.parse(req.query.ua || req.headers['user-agent']);
+		res.send(ua);
+	});
+
+	app.listen(port, function () {
+		if (cluster.worker.id == 1) {
+			process.stdout.write("Server listening on port " + port + "\n");
+		}
+		process.nextTick(function () {
+			process.stdout.write(".");
+			if (cluster.worker.id == forks) {
+				process.nextTick(function () {
+					process.stdout.write(" ready\n");
+				})
+			}
+		})
+	});
+}
